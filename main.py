0adbe576dad2ebba915e6c2d5b38dc9b0b8feb20
Julius Ramshage <JuliusRamshage@lavabit.com>
2023-08-13 22:22:48 +0200

    removed the asycronus functions

--- main.py
+++ main.py
@@ -2,23 +2,20 @@ from handle_requests import get_links
 from handle_requests import get_text
 import sys
 import os
-import asyncio
-import functools
-from multiprocessing import Pool 
 #The main function checks for an argument that was passed along with the calling of the script.
 #if this argument is a file it then read each line from the file and passes it to the 
 # collect_text() function. If not it treats the argument as a URL and passes only that to the collect_text() function
-async def main():
+def main():
     adress_name = sys.argv[1]
     if os.path.exists(str(os.getcwd()) + "/" + adress_name):
         with open(adress_name) as fin:
             for line in fin:
                 print(line)
                 line = line.strip("\n")
-                await collect_text(line)
+                collect_text(line)
 
     else:
-        await collect_text(adress_name)
+        collect_text(adress_name)
 
 
 
@@ -28,21 +25,19 @@ async def main():
 #it will not run it since it's likly a broken link or a link that didn't get edited properly in the 
 #get_links() function. This isn't the best solution since it allows links with https further into the URL 
 #to slip through the detection and can cause an error, however it's unlikely.
-async def collect_text(adress_name):
 
-    links_array = await get_links(adress_name)
+def collect_text(adress_name):
+
+    links_array = get_links(adress_name)
     current_dir = os.getcwd()
     new_dir = current_dir + "/collected_texts/"
     path = os.makedirs(new_dir, exist_ok=True)
     file_name = adress_name.split(".", 1)
     file_name = str(adress_name[1])
     links_array = set(links_array)
-    await get_text(adress_name)
-    with Pool() as pool:
-        for link in links_array:
-            results = pool.imap(await get_text(link), links_array)
-    #tasks = [asyncio.create_task(get_text(link)) for link in links_array ]
-    #done, pending = await asyncio.wait(tasks)
+    get_text(adress_name)
+    for link in links_array:
+        get_text(link)
 
 
     #for link in links_array:
@@ -54,4 +49,4 @@ async def collect_text(adress_name):
 
 if __name__ == '__main__':
         
-    asyncio.run(main())
+    main()
